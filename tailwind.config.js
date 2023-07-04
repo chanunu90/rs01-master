9ec61726c847a47a85e015ee95f174e54a649b49
himediaEUNWOOCHA <vltfhem1206@gmail.com>
2023-07-04 18:13:10 +0900

    0704 리엑트

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'sb-green' : 'bg-white',
+        'sb-gray1' : '#969691',
+        'sb-gary2' : '#F1ECE1'
+      }
+    },
+  },
+  plugins: [],
+}
+
+// #1b3c35
